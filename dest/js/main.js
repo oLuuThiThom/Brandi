b05bcbb8f17a71586067978e10da98a69e68adff
thomblt-1786 <luu.thi.thomb@sun-asterisk.com>
2019-05-08 16:43:30 +0700

    map

--- dest/js/main.js
+++ dest/js/main.js
@@ -760,183 +760,183 @@ $(window).on('load resize', function () {
 function initMap() {
   // Styles a map in night mode.
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.674, lng: -73.945},
+    center: { lat: 40.674, lng: -73.945 },
     zoom: 12,
     styles: [
-    {
+      {
         "featureType": "water",
         "elementType": "geometry",
         "stylers": [
-            {
-                "color": "#e9e9e9"
-            },
-            {
-                "lightness": 17
-            }
+          {
+            "color": "#e9e9e9"
+          },
+          {
+            "lightness": 17
+          }
         ]
-    },
-    {
+      },
+      {
         "featureType": "landscape",
         "elementType": "geometry",
         "stylers": [
-            {
-                "color": "#f5f5f5"
-            },
-            {
-                "lightness": 20
-            }
+          {
+            "color": "#f5f5f5"
+          },
+          {
+            "lightness": 20
+          }
         ]
-    },
-    {
+      },
+      {
         "featureType": "road.highway",
         "elementType": "geometry.fill",
         "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 17
-            }
+          {
+            "color": "#ffffff"
+          },
+          {
+            "lightness": 17
+          }
         ]
-    },
-    {
+      },
+      {
         "featureType": "road.highway",
         "elementType": "geometry.stroke",
         "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 29
-            },
-            {
-                "weight": 0.2
-            }
+          {
+            "color": "#ffffff"
+          },
+          {
+            "lightness": 29
+          },
+          {
+            "weight": 0.2
+          }
         ]
-    },
-    {
+      },
+      {
         "featureType": "road.arterial",
         "elementType": "geometry",
         "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 18
-            }
+          {
+            "color": "#ffffff"
+          },
+          {
+            "lightness": 18
+          }
         ]
-    },
-    {
+      },
+      {
         "featureType": "road.local",
         "elementType": "geometry",
         "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 16
-            }
+          {
+            "color": "#ffffff"
+          },
+          {
+            "lightness": 16
+          }
         ]
-    },
-    {
+      },
+      {
         "featureType": "poi",
         "elementType": "geometry",
         "stylers": [
-            {
-                "color": "#f5f5f5"
-            },
-            {
-                "lightness": 21
-            }
+          {
+            "color": "#f5f5f5"
+          },
+          {
+            "lightness": 21
+          }
         ]
-    },
-    {
+      },
+      {
         "featureType": "poi.park",
         "elementType": "geometry",
         "stylers": [
-            {
-                "color": "#dedede"
-            },
-            {
-                "lightness": 21
-            }
+          {
+            "color": "#dedede"
+          },
+          {
+            "lightness": 21
+          }
         ]
-    },
-    {
+      },
+      {
         "elementType": "labels.text.stroke",
         "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#ffffff"
-            },
-            {
-                "lightness": 16
-            }
+          {
+            "visibility": "on"
+          },
+          {
+            "color": "#ffffff"
+          },
+          {
+            "lightness": 16
+          }
         ]
-    },
-    {
+      },
+      {
         "elementType": "labels.text.fill",
         "stylers": [
-            {
-                "saturation": 36
-            },
-            {
-                "color": "#333333"
-            },
-            {
-                "lightness": 40
-            }
+          {
+            "saturation": 36
+          },
+          {
+            "color": "#333333"
+          },
+          {
+            "lightness": 40
+          }
         ]
-    },
-    {
+      },
+      {
         "elementType": "labels.icon",
         "stylers": [
-            {
-                "visibility": "off"
-            }
+          {
+            "visibility": "off"
+          }
         ]
-    },
-    {
+      },
+      {
         "featureType": "transit",
         "elementType": "geometry",
         "stylers": [
-            {
-                "color": "#f2f2f2"
-            },
-            {
-                "lightness": 19
-            }
+          {
+            "color": "#f2f2f2"
+          },
+          {
+            "lightness": 19
+          }
         ]
-    },
-    {
+      },
+      {
         "featureType": "administrative",
         "elementType": "geometry.fill",
         "stylers": [
-            {
-                "color": "#fefefe"
-            },
-            {
-                "lightness": 20
-            }
+          {
+            "color": "#fefefe"
+          },
+          {
+            "lightness": 20
+          }
         ]
-    },
-    {
+      },
+      {
         "featureType": "administrative",
         "elementType": "geometry.stroke",
         "stylers": [
-            {
-                "color": "#fefefe"
-            },
-            {
-                "lightness": 17
-            },
-            {
-                "weight": 1.2
-            }
+          {
+            "color": "#fefefe"
+          },
+          {
+            "lightness": 17
+          },
+          {
+            "weight": 1.2
+          }
         ]
-    }
-		]
+      }
+    ]
   });
 }
